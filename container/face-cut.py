25cf2bdfe9da58bf7f0d12ce2fdd227b5b352dc6
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2023-01-13 09:42:43 +0300

    init

--- container/face-cut.py
+++ container/face-cut.py
@@ -11,47 +11,46 @@ async def after_server_start(app, loop):
 
 @app.route("/", methods=["POST"])
 async def handler(event):
-    print('Hello I am alive!')
-    # msg = json.loads(event['messages'][0]['details']['message']['body'])
-    # session = boto3.session.Session(region_name='ru-central1')
-    # s3 = session.client(service_name='s3', endpoint_url='https://storage.yandexcloud.net')
-    # img = io.BytesIO()
-    # photo_key = msg['photo_key']
-    # crods = msg['cords']
-    # s3.download_fileobj('cloudphoto', photo_key, img)
+    print(event)
+    msg = json.loads(event['messages'][0]['details']['message']['body'])
+    session = boto3.session.Session(region_name='ru-central1')
+    s3 = session.client(service_name='s3', endpoint_url='https://storage.yandexcloud.net')
+    img = io.BytesIO()
+    photo_key = msg['photo_key']
+    crods = msg['cords']
+    s3.download_fileobj('cloudphoto', photo_key, img)
 
-    # image = Image.open(img)
-    # left = int(cords[0]['x'])
-    # top = int(crods[0]['y'])
-    # right = int(cords[3]['x'])
-    # bottom = int(cords[1]['y'])
-    # cropped = image.crop((left, top, right, bottom))
+    image = Image.open(img)
+    left = int(cords[0]['x'])
+    top = int(crods[0]['y'])
+    right = int(cords[3]['x'])
+    bottom = int(cords[1]['y'])
+    cropped = image.crop((left, top, right, bottom))
 
-    # upload_img = io.BytesIO()
-    # cropped.save(upload_img, 'jpeg')
-    # upload_img.seek(0)
-    # new_photo_key = ''.join([photo_key.split('.')[0], str(left), str(top), str(right), str(bottom), '.jpg'])
-    # s3.upload_fileobj(upload_img, 'itis-2022-2023-vvot45-faces', new_photo_key)
+    upload_img = io.BytesIO()
+    cropped.save(upload_img, 'jpeg')
+    upload_img.seek(0)
+    new_photo_key = ''.join([photo_key.split('.')[0], str(left), str(top), str(right), str(bottom), '.jpg'])
+    s3.upload_fileobj(upload_img, 'itis-2022-2023-vvot45-faces', new_photo_key)
 
-    # endpoint = ''
-    # path = ''
-    # credentials = ydb.iam.MetadataUrlCredentials()
-    # driver_config = ydb.DriverConfig(
-    #     endpoint, path, credentials=credentials
-    # )
-    # driver = ydb.Driver(driver_config)
+    endpoint = ''
+    path = ''
+    credentials = ydb.iam.MetadataUrlCredentials()
+    driver_config = ydb.DriverConfig(
+        endpoint, path, credentials=credentials
+    )
+    driver = ydb.Driver(driver_config)
     
-    # query = f"""
-    #     PRAGMA TablePathPrefix("{os.environ['DB_PATH']}");
-    #     INSERT INTO {os.environ['DB_NAME']} (original_photo, face_photo)
-    #     VALUES ('{photo_key}', '{new_photo_key}');
-    # """
-    # session = driver.table_client.session().create()
-    # session.transaction().execute(query, commit_tx=True)
-    # session.closing()
+    query = f"""
+        PRAGMA TablePathPrefix("{os.environ['DB_PATH']}");
+        INSERT INTO {os.environ['DB_NAME']} (original_photo, face_photo)
+        VALUES ('{photo_key}', '{new_photo_key}');
+    """
+    session = driver.table_client.session().create()
+    session.transaction().execute(query, commit_tx=True)
+    session.closing()
 
     return text('Ok')
 
 if __name__ == "__main__":
-    sys.stdout.write('Hello i am here!')
     app.run(host='0.0.0.0', port=int(os.environ['PORT']), motd=False, access_log=False)
